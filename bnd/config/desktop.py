8ff56aefbf4dd1b6f17ffc47f96d39f6d82195d5
sambhaji <kolate.sambhaji@gmail.com>
2018-03-19 20:39:59 +0530

    desktop icons

--- bnd/config/desktop.py
+++ bnd/config/desktop.py
@@ -10,5 +10,30 @@ def get_data():
 			"icon": "fa fa-subway",
 			"type": "module",
 			"label": _("BND")
-		}
+		},
+		{
+			"module_name": "Store",
+			"_doctype": "Store",
+			"color": "#0f9155",
+			"icon": "fa fa-truck",
+			"type": "link",
+			"link": "List/Store"
+		},
+
+		{
+			"module_name": "Device",
+			"_doctype": "Device",
+			"color": "#cb3fcd",
+			"icon": "fa fa-user-circle-o",
+			"type": "link",
+			"link": "List/Device"
+		},
+		{
+			"module_name": "Shift Time",
+			"_doctype": "Shift Time",
+			"color": "#600080",
+			"icon": "fa fa-user",
+			"type": "link",
+			"link": "List/Shift Time"
+		},
 	]
