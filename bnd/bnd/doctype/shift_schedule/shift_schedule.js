dc4c6738f4b2b25964179bf206c1f3cad7f5c8cf
sambhaji <kolate.sambhaji@gmail.com>
2018-03-26 20:18:58 +0530

    added

--- bnd/bnd/doctype/shift_schedule/shift_schedule.js
+++ bnd/bnd/doctype/shift_schedule/shift_schedule.js
@@ -1,8 +1,6 @@
 // Copyright (c) 2018, Digitalprizm and contributors
 // For license information, please see license.txt
 cur_frm.add_fetch('employee', 'employee_name', 'employee_name');
-cur_frm.add_fetch('employee', 'employee', 'employee_id');
-
 cur_frm.add_fetch('shift_time', 'start_time', 'start_time');
 cur_frm.add_fetch('shift_time', 'end_time', 'end_time');
 
