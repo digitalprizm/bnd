7f2501fde2b2810fc0756aca72ee3b050bcc8a07
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-03-27 11:56:37 +0530

    employee name auto fetch

--- bnd/custom_script/attendance.js
+++ bnd/custom_script/attendance.js
@@ -1,7 +1,15 @@
 
+cur_frm.add_fetch('employee', 'company', 'company');
+cur_frm.add_fetch('employee', 'employee_name', 'employee_name');
 frappe.ui.form.on("Attendance", "validate", function(frm) { 
-    console.log("please write script here");
-    /*frm.set_df_property("in_time", "read_only",1);
-	frm.set_df_property("out_time", "read_only",1);*/
-    
+
  });
+
+/*def validate_duplicate_record(self):
+		res = frappe.db.sql("""select name from `tabAttendance` where employee = %s and attendance_date = %s
+			and name != %s and docstatus = 1""",
+			(self.employee, self.attendance_date, self.name))
+		if res:
+			frappe.throw(_("Attendance for employee {0} is already marked").format(self.employee))
+
+		set_employee_name(self)*/
