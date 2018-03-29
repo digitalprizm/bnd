742520a0f2b47858b6846f8bddd53fa8fb07eea3
vishal <vishal@digitalprizmglobal.com>
2018-03-29 11:12:14 +0530

    report on shift schedule exception

---
+++ bnd/bnd/report/shift_schedule_exception/shift_schedule_exception.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2016, Digitalprizm and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["shift schedule exception"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"reqd": 1
+		},		
+		{
+			"fieldname": "store",
+			"label": __("Store"),
+			"fieldtype": "Link",
+			"options": "Store",
+		},
+		{
+			"fieldname":"employee",
+			"label":__("Employee"),
+			"fieldtype":"Link",
+			"options": "Employee",
+		}
+
+	]
+}
