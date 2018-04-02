63a4448638504a8e66091ae0952a50ee225c37f4
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-04-02 18:23:12 +0530

    remove test demo

--- bnd/bnd/doctype/attendance_violation/attendance_violation.py
+++ bnd/bnd/doctype/attendance_violation/attendance_violation.py
@@ -36,8 +36,7 @@ class AttendanceViolation(Document):
 		frappe.msgprint("New Attendance {0} is created".format(attendance_doc.name))
 
 	def on_cancel(self):
-		frappe.msgprint("test")
-		frappe.msgprint("demo")
+		pass
 
 	def validate_duplicate_record(self):
 		res = frappe.db.sql("""select name 
