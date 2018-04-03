b554bd8621af8f7726ecaf101867c5d91b8397bc
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-04-03 20:33:20 +0530

    shift schedule exception update

--- bnd/bnd/doctype/shift_schedule_exception/shift_schedule_exception.py
+++ bnd/bnd/doctype/shift_schedule_exception/shift_schedule_exception.py
@@ -7,4 +7,17 @@ import frappe
 from frappe.model.document import Document
 
 class ShiftScheduleException(Document):
-	pass
+	def validate(self):
+		self.get_shift_schedule()
+
+	def get_shift_schedule(self):
+		data=frappe.db.sql("""select employee, employee_name, 
+			shift_time,attendance_date, store 
+			from `tabShift Schedule` 
+			where employee='{0}' and attendance_date ='{1}'and docstatus='1' limit 1
+		 """.format(self.employee,self.attendance_date), as_dict=1)
+		if data:
+			self.old_store_location = data[0].store
+			self.shift_schedule_old_time = data[0].shift_time
+		else :
+			frappe.throw("Shift schedule is Not found")
