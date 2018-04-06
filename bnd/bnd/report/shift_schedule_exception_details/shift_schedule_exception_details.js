dc765a72f5f7e13670694700e95bb17b922f2fec
pradnya2196 <pgole21@gmail.com>
2018-04-06 16:56:24 +0530

    added

--- bnd/bnd/report/shift_schedule_exception_details/shift_schedule_exception_details.js
+++ bnd/bnd/report/shift_schedule_exception_details/shift_schedule_exception_details.js
@@ -1,6 +1,5 @@
 // Copyright (c) 2016, Digitalprizm and contributors
 // For license information, please see license.txt
-// eslint-disable /
 
 frappe.query_reports["Shift Schedule Exception Details"] = {
 	"filters": [
