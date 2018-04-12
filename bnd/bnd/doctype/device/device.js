159a5424c3c5c57d5873c953104650909ee61d4c
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-04-12 12:55:41 +0530

    add fetch added on device

--- bnd/bnd/doctype/device/device.js
+++ bnd/bnd/doctype/device/device.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2018, Digitalprizm and contributors
 // For license information, please see license.txt
-
+cur_frm.add_fetch('store', 'store_name', 'store_name');
 frappe.ui.form.on('Device', {
 	refresh: function(frm) {
 
