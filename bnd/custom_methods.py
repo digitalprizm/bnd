9eeee2d188b7a00249a392051d3c49ebf7af44c2
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-05-01 11:23:46 +0530

    remove msg print

--- bnd/custom_methods.py
+++ bnd/custom_methods.py
@@ -9,7 +9,6 @@ def salary_slip(doc,method):
 	query="""Select sum(deduction_days), sum(deduction_amount)
 	from `tabAttendance Violation` where (attendance_date between '{0}' and '{1}') and employee='{2}'""".format(doc.start_date, doc.end_date, doc.employee)
 	deduction_data = frappe.db.sql(query,as_list=1,debug=1)
-	frappe.msgprint(deduction_data)
 	if deduction_data:
 		deduction = deduction_data[0]
 		doc.deduction_days = deduction[0]
