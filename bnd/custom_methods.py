66e43318a75d9048e35c77e74eef9fe0f5ae69a6
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-05-01 11:22:20 +0530

    chnages in script

--- bnd/custom_methods.py
+++ bnd/custom_methods.py
@@ -6,13 +6,13 @@ from frappe import _
 
 @frappe.whitelist()
 def salary_slip(doc,method):
-	query="""Select deduction_days, deduction_amount
+	query="""Select sum(deduction_days), sum(deduction_amount)
 	from `tabAttendance Violation` where (attendance_date between '{0}' and '{1}') and employee='{2}'""".format(doc.start_date, doc.end_date, doc.employee)
-
 	deduction_data = frappe.db.sql(query,as_list=1,debug=1)
+	frappe.msgprint(deduction_data)
 	if deduction_data:
 		deduction = deduction_data[0]
 		doc.deduction_days = deduction[0]
 		doc.deduction_amount = deduction[1]
-		frappe.msgprint('Deduction days:'+ str(deduction[0]))
-		frappe.msgprint('Deduction amount:'+ str(deduction[1]))
+		frappe.msgprint('Deduction days:'+ str(doc.deduction_days) )
+		frappe.msgprint('Deduction amount:'+ str(doc.deduction_amount) )
