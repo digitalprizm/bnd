56c77cb7021acee787b04facfc133e193e7a9919
avudev <dev@avu.net.in>
2018-05-10 11:29:58 +0530

    suvarna

---
+++ bnd/bnd/report/employees_information/employees_information.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, Digitalprizm and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Employees Information"] = {
+	"filters": [
+{
+			"fieldname":"employee_name",
+			"label": __("Employee Name"),
+			"fieldtype": "Data",
+			
+		}
+	]
+}
