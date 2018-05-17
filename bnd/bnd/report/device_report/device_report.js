58b02d41b678a3bb2646b2d4e6f295fb21f3492e
avudev <dev@avu.net.in>
2018-05-17 17:13:08 +0530

    Pranali

---
+++ bnd/bnd/report/device_report/device_report.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2016, Digitalprizm and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+
+
+
+
+frappe.query_reports["Device Report"] = {
+	"filters": [
+{
+			"fieldname":"device_name",
+			"label": __("Device Name"),
+			"fieldtype": "Data",
+			
+		},
+{
+			"fieldname":"ip_address",
+			"label": __("IP Address"),
+			"fieldtype": "Data",
+			
+		}
+
+	]
+}
+
---
+++ bnd/bnd/report/store_report/store_report.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2016, Digitalprizm and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Store Report"] = {
+	"filters": [
+	{
+			"fieldname":"store_name",
+			"label": __("Store Name"),
+			"fieldtype": "Data",
+			
+		}
+
+	]
+}
+
+
