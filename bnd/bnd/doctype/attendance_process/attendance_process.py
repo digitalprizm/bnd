37b5d6366147c15d1160d28909538c07d3be669d
avudev <dev@avu.net.in>
2018-05-19 18:55:30 +0530

    pranali

--- bnd/bnd/doctype/attendance_process/attendance_process.py
+++ bnd/bnd/doctype/attendance_process/attendance_process.py
@@ -17,23 +17,3 @@ class AttendanceProcess(Document):
 		alist=load_data(process_date)
 		self.get("__onload").attendance_list = alist
 
-
-@frappe.whitelist()
-def calling_attendance_api(process_date,enroll_number):
-	r = requests.get('http://120.88.36.10:560/subtest/api/Aprocess?_date={0}&_enroll={1}'.format(process_date,enroll_number))
-	api=r.text
-	if api=="\"[]\"":
-		d=[{"employee":"%s"%(enroll_number),"message":"no punch found","status":"unsucessfull"}]
-		api=json.dumps(json.dumps(d))
-	return api
-
-
-
-
-@frappe.whitelist()
-def calling_attendance_date_api(process_date):
-	r = requests.get('http://120.88.36.10:560/subtest/api/Aprocess?_date={0}'.format(process_date))
-	api=r.text
-	return api
-
-
