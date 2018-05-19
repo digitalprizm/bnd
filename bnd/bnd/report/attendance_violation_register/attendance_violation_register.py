1a76693760b494d9c99214831c4cb3d80788916e
avudev <dev@avu.net.in>
2018-05-19 17:44:36 +0530

    added search bar in attendance process page -by pranali

---
+++ bnd/bnd/report/attendance_violation_register/attendance_violation_register.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+import frappe
+
+def execute(filters=None):
+	if not filters: filters = {'employee_name':''}
+
+
+
+	dict=frappe.db.sql("CALL `violation_report`('{0}')".format(filters['employee_name']), as_dict=1);
+
+
+	columns = get_columns(dict)
+	data= get_values_list(dict)
+	return columns, data
+
+def get_columns(data):
+	column = []
+	if len(data)!=0:
+		for _key in sorted(data[0].keys()):
+			_charidx = _key.find('#') + 1 if _key.find('#') > 0 else 0
+			column.append(_key[_charidx:])
+	return column
+
+
+def get_values_list(data):
+	rows = []
+	if len(data)!=0:
+		for idx in range(0,len(data)):
+			val = []
+			for _key in sorted(data[0].keys()):
+				val.append(data[idx][_key])
+			rows.append(val)		
+	return rows
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
